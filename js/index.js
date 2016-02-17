09c9979b1f5d983d772c58e0b7b918ba62a67f32
AnnonIp <j.martin.utn@gmail.com>
2016-02-17 03:03:47 -0300

    AngularJs implementation and new section page

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
-$(document).ready(function(){               
-  console.log('Meh, solo un mensaje oculto re cool.');
-});
+var app = angular.module('annonipModule', ['ui.router']);
+
+	app.controller('AnnonipController', ['$scope', function($scope) {
+		$scope.$on('$viewContentLoaded', function(event) {
+			$('select').material_select();
+		});
+	}]);
+
+	app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+		  // For any unmatched url, redirect to /
+		  $urlRouterProvider.otherwise("/");
+
+		  // Now set up the states
+		  $stateProvider
+		    .state('root', {
+	      		url: "/",
+				views: 
+				{
+			        "header": { templateUrl: "views/header.html" },
+			        "content": { templateUrl: "views/content.html" },
+			        "footer": { templateUrl: "views/footer.html" }
+
+		      	}		    
+			})
+		    .state('discount', {
+	      		url: "/discount",
+				views: 
+				{
+			        "content": { 
+			        	templateUrl: "views/discount.html",
+			        	controller: "AnnonipController"
+			    	},
+			        "footer": { templateUrl: "views/footer.html" }
+
+		      	}	
+			});
+
+			$locationProvider.html5Mode(true)
+
+	});
